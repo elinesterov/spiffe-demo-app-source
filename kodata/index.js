a32c27be320726ea85508e3fae2d36eefb6c5474
Eli Nesterov <eli.nesterov@gmail.com>
2023-03-13 20:56:48 -0700

    add jwt.io-style jwt-svid info

--- kodata/index.js
+++ kodata/index.js
@@ -1,23 +1,60 @@
 const getJwtButton = document.getElementById('getJwtButton');
 const getX509Button = document.getElementById('getX509Button');
 const getTrustBundleButton = document.getElementById('getTrustBundleButton');
-const output = document.getElementById('output');
-const parsedCert = document.getElementById('parsed-cert');
+let output = document.getElementById('output');
+let parsedCert = document.getElementById('parsed-cert');
 
-function clearParsedCert() {
+function clearOutput() {
+  output.innerHTML = `<div></div>`;
   parsedCert.innerHTML = '';
 }
 
 getJwtButton.addEventListener('click', async () => {
   try {
-    clearParsedCert();
+    clearOutput();
     const response = await fetch('/api/getjwtsvid');
     if (!response.ok) {
       const error = await response.text();
       throw new Error(error);
     }
-    const jwt = await response.text();
-    output.innerHTML = `<div>${jwt}</div>`;
+    const jsonResponse = await response.json();
+    const token = jsonResponse.token;
+
+    // Create container elements for encoded and decoded JWT
+    const encodedContainer = document.createElement('div');
+    const decodedContainer = document.createElement('div');
+
+    // Set CSS properties for container elements
+    encodedContainer.style.cssText = 'float:left; max-width:50%;';
+    decodedContainer.style.cssText = 'float:right; width:50%;';
+
+    // Display encoded JWT in first column
+    const encodedHeader = document.createElement('h3');
+    encodedHeader.textContent = 'Encoded';
+
+    const encodedText = document.createElement('div');
+    encodedText.classList.add('token');
+    encodedText.style.wordWrap = 'break-word';
+    encodedText.textContent = token;
+
+    encodedContainer.appendChild(encodedHeader);
+    encodedContainer.appendChild(encodedText);
+
+    // Decode JWT and display decoded contents in second column
+    const decodedHeader = document.createElement('h3');
+    decodedHeader.textContent = 'Decoded';
+
+    const decodedText = document.createElement('pre');
+    decodedText.classList.add('token');
+    decodedText.textContent = JSON.stringify(JSON.parse(atob(token.split('.')[1])), null, 2);
+    decodedContainer.appendChild(decodedHeader);
+    decodedContainer.appendChild(decodedText);
+
+    // Add both columns to parsed-cert element
+    let parsedCert = document.getElementById('parsed-cert');
+    parsedCert.appendChild(encodedContainer);
+    parsedCert.appendChild(decodedContainer);
+
   } catch (error) {
     console.error(error);
     output.innerHTML = `<div>Error: ${error.message}</div>`;
@@ -26,7 +63,7 @@ getJwtButton.addEventListener('click', async () => {
 
 getX509Button.addEventListener('click', async () => {
   try {
-    clearParsedCert();
+    clearOutput();
     const response = await fetch('/api/getx509svid');
     if (!response.ok) {
       const error = await response.text();
@@ -42,7 +79,7 @@ getX509Button.addEventListener('click', async () => {
 
 getTrustBundleButton.addEventListener('click', async () => {
   try {
-    clearParsedCert();
+    clearOutput();
     const response = await fetch('/api/gettrustbundle');
     if (!response.ok) {
       const error = await response.text();
