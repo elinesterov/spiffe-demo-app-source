41d3f5b1510d7ff0d7ecf4d10c8e6f5f6dc6c569
Ismael <ismael.kazzouzi@gmail.com>
2024-10-09 18:48:31 +0100

    adfs test, proper audience

--- api/api.go
+++ api/api.go
@@ -39,7 +39,7 @@ func (a *API) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 func (a *API) GetJwtHandler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	svid, err := a.client.FetchJWTSVID(a.ctx, jwtsvid.Params{
-		Audience: "spiffe://mywicklow.com/kind-kind/ns/spiffe-demo/sa/spiffe-demo-app",
+		Audience: "https://adfs.mydomain2.com/adfs/oauth2/token",
 	//	ExtraAudiences: []string{
 	//		"spiffe://example.org/foo",
 	//		"spiffe://acme.com/bar",
